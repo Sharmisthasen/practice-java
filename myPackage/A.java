1285df55bf78464d2fb30ef12de15ba33a589bb1
cools <cools@192.168.0.13>
2017-05-24 00:00:16 -0400

    Inner class testing

--- myPackage/A.java
+++ myPackage/A.java
@@ -2,7 +2,13 @@ package myPackage;
 
 class A{
 public A(){
-	System.out.print("hello");
+	System.out.print("hello i am Class A\n");
+}
+
+protected void ProtetedMethod(){
+	String s = null;
+	s="I am inside class A , method name ProtectedMethod";
+	System.out.println(s);
 }
 public static void main(String args[]){
 	System.out.println("hello world");
--- myPackage/ArrayTest.java
+++ myPackage/ArrayTest.java
@@ -3,6 +3,11 @@ package myPackage;
 import java.util.ArrayList;
 
 public class ArrayTest {
+	
+	int i = 10;
+	public void printI(){
+		System.out.println("In class ArrayTest, inside method printI"+i);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
